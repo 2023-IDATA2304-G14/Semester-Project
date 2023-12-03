2e83e792c054a8a6cc637dead04ea41131ad0127
larsbarstad <lmlovold@stud.ntnu.no>
2023-12-03 22:07:58 +0100

    removed mistakes

--- src/main/java/no/ntnu/gui/greenhouse/MainGreenhouseGuiWindow.java
+++ src/main/java/no/ntnu/gui/greenhouse/MainGreenhouseGuiWindow.java
@@ -13,12 +13,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import no.ntnu.greenhouse.SensorReading;
-import no.ntnu.gui.common.SensorPane;
-import no.ntnu.greenhouse.Sensor;
-import java.util.List;
-import java.util.stream.Collectors;
-
 
 /**
  * The main GUI window for greenhouse simulator.
@@ -27,62 +21,16 @@ public class MainGreenhouseGuiWindow extends Scene {
   public static final int WIDTH = 300;
   public static final int HEIGHT = 300;
 
-  // Dashboard labels
-  private static Label temperatureLabel = new Label("Temperature: -- °C");
-  private static Label humidityLabel = new Label("Humidity: -- %");
-
-  private SensorPane temperatureSensorPane;
-  private SensorPane humiditySensorPane;
-
   public MainGreenhouseGuiWindow() {
-
     super(createMainContent(), WIDTH, HEIGHT);
-
-    temperatureSensorPane = new SensorPane();
-    humiditySensorPane = new SensorPane();
-
-    ((VBox) this.getRoot()).getChildren().addAll(temperatureSensorPane, humiditySensorPane);
   }
 
   private static Parent createMainContent() {
-    VBox container = new VBox(
-            createInfoLabel(),
-            createDashboard(temperatureLabel, humidityLabel),
-            createMasterImage(),
-            createCopyrightNotice());
+    VBox container = new VBox(createInfoLabel(), createMasterImage(), createCopyrightNotice());
     container.setPadding(new Insets(20));
     container.setAlignment(Pos.CENTER);
     container.setSpacing(5);
     return container;
-  }
-  public void updateSensorData(List<Sensor> updatedSensors) {
-    // Filter and update temperature sensors
-    List<SensorReading> temperatureReadings = updatedSensors.stream()
-            .filter(sensor -> sensor.getType().equals("Temperature"))
-            .map(Sensor::getReading) // Use getReading or getValue here?
-            .collect(Collectors.toList());
-    if (!temperatureReadings.isEmpty()) {
-      temperatureSensorPane.update(temperatureReadings);
-    }
-
-    // Filter and update humidity sensors
-    List<SensorReading> humidityReadings = updatedSensors.stream()
-            .filter(sensor -> sensor.getType().equals("Humidity"))
-            .map(Sensor::getReading) // Use getReading or getValue here?
-            .collect(Collectors.toList());
-    if (!humidityReadings.isEmpty()) {
-      humiditySensorPane.update(humidityReadings);
-    }
-
-  }
-
-  private static Node createDashboard(Label tempLabel, Label humidLabel) {
-    VBox dashboard = new VBox();
-    dashboard.setAlignment(Pos.CENTER);
-    dashboard.setSpacing(10);
-
-    dashboard.getChildren().addAll(tempLabel, humidLabel);
-    return dashboard;
   }
 
   private static Label createInfoLabel() {
