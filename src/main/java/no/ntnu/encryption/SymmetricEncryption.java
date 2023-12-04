de743495aa0d5ce5852d5f590fec3f8dbbeb5d93
Anders Lund <a_lund_01@hotmail.com>
2023-12-04 22:53:16 +0100

    Fixes sonarlint issues

--- src/main/java/no/ntnu/encryption/SymmetricEncryption.java
+++ src/main/java/no/ntnu/encryption/SymmetricEncryption.java
@@ -1,52 +1,54 @@
 package no.ntnu.encryption;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.*;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 
-public class SymmetricEncryption {
+class SymmetricEncryption {
 
-  public static byte[] encryptMessage(String messageToEncrypt, String passPhrase) throws Exception {
-
-    SecretKey secretKey = generateSecretKeyFromPassword(passPhrase);
-    Cipher cipher = Cipher.getInstance("AES");
-    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-    byte[] encryptedBytes = cipher.doFinal(messageToEncrypt.getBytes());
-
-    return encryptedBytes;
+  /**
+   * This class should not be instantiated.
+   */
+  private SymmetricEncryption() {
   }
 
-  public static String decryptMessage(byte[] encryptedMessage, String passPhrase) throws Exception {
+  public static byte[] encryptMessage(String messageToEncrypt, String passPhrase) throws IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
     SecretKey secretKey = generateSecretKeyFromPassword(passPhrase);
-    Cipher cipher = Cipher.getInstance("AES");
+    Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+    return cipher.doFinal(messageToEncrypt.getBytes());
+  }
+
+  public static String decryptMessage(byte[] encryptedMessage, String passPhrase) throws IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, InvalidKeyException {
+    SecretKey secretKey = generateSecretKeyFromPassword(passPhrase);
+    Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
     cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
     byte[] decryptBytes = cipher.doFinal(encryptedMessage);
-    String decryptedMessage = new String(decryptBytes);
 
-    return decryptedMessage;
+    return new String(decryptBytes);
   }
 
 
-  private static SecretKey generateSecretKeyFromPassword(String password) throws Exception {
+  private static SecretKey generateSecretKeyFromPassword(String password) throws IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException {
     byte[] salt = "SecureSeaSalt!".getBytes();
     int iterations = 10000;
-    SecretKey secretKey = generateSecretKeyFromPassword(password, salt, iterations);
 
-    return secretKey;
+    return generateSecretKeyFromPassword(password, salt, iterations);
   }
 
-  private static SecretKey generateSecretKeyFromPassword(String password, byte[] salt, int iterations) throws Exception {
+  private static SecretKey generateSecretKeyFromPassword(String password, byte[] salt, int iterations) throws IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException {
     SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
     KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, iterations, 256);
     SecretKey tmp = secretKeyFactory.generateSecret(keySpec);
-    SecretKey secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
 
-    return secretKey;
+    return new SecretKeySpec(tmp.getEncoded(), "AES");
   }
 
 
--- src/test/java/no/ntnu/encryption/SymmetricEncryptionTest.java
+++ src/test/java/no/ntnu/encryption/SymmetricEncryptionTest.java
@@ -9,11 +9,10 @@ class SymmetricEncryptionTest {
   //Test if the message can be encrypted and decrypted with the right passPhrase
   @Test
   void testEncryptionWithCorrectPassPhrase() throws Exception {
-    SymmetricEncryption symmetricEncryption = new SymmetricEncryption();
     String messageToEncrypt = "Super Secret Message";
     String passPhrase = "B3stPassPhr4se";
-    byte[] encryptedMessage = symmetricEncryption.encryptMessage(messageToEncrypt, passPhrase);
-    String decryptedMessage = symmetricEncryption.decryptMessage(encryptedMessage, passPhrase);
+    byte[] encryptedMessage = SymmetricEncryption.encryptMessage(messageToEncrypt, passPhrase);
+    String decryptedMessage = SymmetricEncryption.decryptMessage(encryptedMessage, passPhrase);
 
     assertEquals(messageToEncrypt, decryptedMessage);
   }
@@ -21,13 +20,12 @@ class SymmetricEncryptionTest {
   //Test if error (Exception) are thrown if the passPhrase is wrong
   @Test
   void testEncryptionWithNotCorrectPassPhrase() throws Exception {
-    SymmetricEncryption symmetricEncryption = new SymmetricEncryption();
     String messageToEncrypt = "Super Secret Message";
     String passPhrase1 = "B3stPassPhr4se";
     String passPhrase2 = "B3stP4ssPhr4s3";
-    byte[] encryptedMessage = symmetricEncryption.encryptMessage(messageToEncrypt, passPhrase1);
+    byte[] encryptedMessage = SymmetricEncryption.encryptMessage(messageToEncrypt, passPhrase1);
 
-    assertThrows(Exception.class, () -> symmetricEncryption.decryptMessage(encryptedMessage, passPhrase2));
+    assertThrows(Exception.class, () -> SymmetricEncryption.decryptMessage(encryptedMessage, passPhrase2));
   }
 
 
