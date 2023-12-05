dbecf1acb7347be4ca546722b18963e5ebb9338f
larsbarstad <lmlovold@stud.ntnu.no>
2023-12-05 21:04:48 +0100

    pushed mistakes and regretted life

--- src/main/java/no/ntnu/gui/greenhouse/MainGreenhouseGuiWindow.java
+++ src/main/java/no/ntnu/gui/greenhouse/MainGreenhouseGuiWindow.java
@@ -27,17 +27,35 @@ public class MainGreenhouseGuiWindow extends Scene {
   public static final int WIDTH = 500;
   public static final int HEIGHT = 500;
   private final Map<Integer, Parent> nodes = new HashMap<>();
+  private BorderPane container;
+
 
   public MainGreenhouseGuiWindow() {
-    super(createMainContent(), WIDTH, HEIGHT);
+    super(new BorderPane(), WIDTH, HEIGHT);
+    container = (BorderPane) this.getRoot();
+    mainWindow();
+  }
+  private void mainWindow() {
+    // Create a new VBox for organizing nodes
+    VBox leftVBox = new VBox(10);
+
+    // Iterate over the HashMap and add each node's GUI component to the VBox
+    for (Map.Entry<Integer, Parent> entry : nodes.entrySet()) {
+      leftVBox.getChildren().add(entry.getValue());
+    }
+    container.setLeft(leftVBox);
+    container.setRight(createInfoLabel());
+    container.setCenter(createMasterImage());
+
+  }
+  public void addNode(int nodeId, Parent nodeGui) {
+    nodes.put(nodeId, nodeGui);
+    mainWindow();
   }
 
-  private static Parent createMainContent() {
-    BorderPane container = new BorderPane();
-    container.setPadding(new Insets(20));
-    container.setLeft(createMasterImage());
-    container.setRight(createInfoLabel());
-    return container;
+  public void removeNode(int nodeId) {
+    nodes.remove(nodeId);
+    mainWindow();
   }
   private static Label createInfoLabel() {
     Label l = new Label("Close this window to stop the whole simulation");
@@ -45,38 +63,6 @@ public class MainGreenhouseGuiWindow extends Scene {
     l.setPadding(new Insets(0, 0, 10, 0));
     return l;
   }
-
-  public void addNode(int nodeId, Parent nodeGui) {
-    nodes.put(nodeId, nodeGui);
-    updateGui();
-  }
-
-  private void updateGui() {
-    // Create a new VBox for organizing nodes
-    VBox leftVBox = new VBox(10); // VBox with spacing of 10
-
-    // Iterate over the HashMap and add each node's GUI component to the VBox
-    for (Map.Entry<Integer, Parent> entry : nodes.entrySet()) {
-      leftVBox.getChildren().add(entry.getValue());
-    }
-
-    // Create a new BorderPane for the layout
-    BorderPane borderPane = new BorderPane();
-    borderPane.setLeft(leftVBox); // Set the VBox in the left region of the BorderPane
-
-    // Optionally, you can set other regions or leave them empty
-    // borderPane.setTop(someTopNode);
-    // borderPane.setCenter(someCenterNode);
-    // borderPane.setRight(someRightNode);
-    // borderPane.setBottom(someBottomNode);
-
-    // Set the BorderPane or updated content to the appropriate part of your scene
-    BorderPane container = (BorderPane) this.getRoot();
-    container.setCenter(borderPane);
-  }
-
-
-
   private static Node createMasterImage() {
     Node node;
     try {
