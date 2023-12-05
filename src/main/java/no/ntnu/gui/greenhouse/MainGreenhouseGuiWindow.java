04d3158c12fb585bbce33e294792db92859ed613
larsbarstad <lmlovold@stud.ntnu.no>
2023-12-05 13:37:32 +0100

    made changes to node placement in VBox

--- src/main/java/no/ntnu/gui/greenhouse/MainGreenhouseGuiWindow.java
+++ src/main/java/no/ntnu/gui/greenhouse/MainGreenhouseGuiWindow.java
@@ -52,47 +52,29 @@ public class MainGreenhouseGuiWindow extends Scene {
   }
 
   private void updateGui() {
-    BorderPane mainWindow = new BorderPane();
+    // Create a new VBox for organizing nodes
+    VBox leftVBox = new VBox(10); // VBox with spacing of 10
 
-    VBox leftVBox = new VBox(10);
-    VBox rightVBox = new VBox(10);
-    HBox topHBox = new HBox(10);
-    HBox bottomHBox = new HBox(10);
-
-    // Iterate over the HashMap and add nodes to different regions
-    int count = 0;
+    // Iterate over the HashMap and add each node's GUI component to the VBox
     for (Map.Entry<Integer, Parent> entry : nodes.entrySet()) {
-      // Example logic for placing nodes in different regions
-      switch (count % 4) {
-        case 0:
-          leftVBox.getChildren().add(entry.getValue());
-          break;
-        case 1:
-          rightVBox.getChildren().add(entry.getValue());
-          break;
-        case 2:
-          topHBox.getChildren().add(entry.getValue());
-          break;
-        case 3:
-          bottomHBox.getChildren().add(entry.getValue());
-          break;
-      }
-      count++;
+      leftVBox.getChildren().add(entry.getValue());
     }
 
-    // Set the VBox and HBox in the respective regions of the BorderPane
-    mainWindow.setLeft(leftVBox);
-    mainWindow.setRight(rightVBox);
-    mainWindow.setTop(topHBox);
-    mainWindow.setBottom(bottomHBox);
+    // Create a new BorderPane for the layout
+    BorderPane borderPane = new BorderPane();
+    borderPane.setLeft(leftVBox); // Set the VBox in the left region of the BorderPane
 
-    // Optionally, you can set a central node if needed
-    // borderPane.setCenter(someCentralNode);
+    // Optionally, you can set other regions or leave them empty
+    // borderPane.setTop(someTopNode);
+    // borderPane.setCenter(someCenterNode);
+    // borderPane.setRight(someRightNode);
+    // borderPane.setBottom(someBottomNode);
 
     // Set the BorderPane or updated content to the appropriate part of your scene
     BorderPane container = (BorderPane) this.getRoot();
-    container.setCenter(mainWindow);
+    container.setCenter(borderPane);
   }
+
 
 
   private static Node createMasterImage() {
