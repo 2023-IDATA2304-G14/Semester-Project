236606f69f6be7d563723ec8fccc55ee3783b8bf
Anders Lund <a_lund_01@hotmail.com>
2023-12-07 15:24:33 +0100

    Improves MessageParameterizer

--- src/main/java/no/ntnu/message/MessageParameterizer.java
+++ src/main/java/no/ntnu/message/MessageParameterizer.java
@@ -1,67 +1,110 @@
 package no.ntnu.message;
 
-import java.util.regex.Pattern;
-
 public class MessageParameterizer {
   private String prefix;
   private String nodeId;
-  private String actuatorId;
-  private String sensorId;
+  private String itemId;
   private String type;
   private String value;
-  public static final String NODE_ID_PARAMETER = "nodeId";
-  public static final String ACTUATOR_ID_PARAMETER = "actuatorId";
-  public static final String SENSOR_ID_PARAMETER = "sensorId";
-  public static final String TYPE_PARAMETER = "type";
-  public static final String VALUE_PARAMETER = "value";
+  public static final String NODE_ID_PARAMETER = "nId";
+  public static final String ITEM_ID_PARAMETER = "iId";
+  public static final String TYPE_PARAMETER = "t";
+  public static final String VALUE_PARAMETER = "v";
 
+    /**
+     * Creates a new MessageParameterizer with the given prefix.
+     * @param prefix The prefix of the message.
+     */
   public MessageParameterizer(String prefix) {
     this.prefix = prefix;
   }
 
+  /**
+   * Sets the nodeId parameter.
+   * @param nodeId The nodeId to set.
+   * @return The MessageParameterizer object.
+   */
   public MessageParameterizer setNodeId(String nodeId) {
     this.nodeId = nodeId;
     return this;
   }
 
+  /**
+   * Gets the nodeId parameter.
+   * @return The nodeId parameter.
+   */
   public String getNodeId() {
     return nodeId;
   }
 
-  public MessageParameterizer setActuatorId(String actuatorId) {
-    this.actuatorId = actuatorId;
+  /**
+   * Sets the itemId parameter. Either a sensorId or an actuatorId depending on the message.
+   * @param itemId The itemId to set.
+   * @return The MessageParameterizer object.
+   */
+  public MessageParameterizer setItemId(String itemId) {
+    this.itemId = itemId;
     return this;
   }
 
-  public String getActuatorId() {
-    return actuatorId;
+  /**
+   * Gets the itemId parameter. Either a sensorId or an actuatorId depending on the message.
+   * @return The itemId parameter.
+   */
+  public String getItemId() {
+    return itemId;
   }
 
-  public MessageParameterizer setSensorId(String sensorId) {
-    this.sensorId = sensorId;
-    return this;
-  }
-
-  public String getSensorId() {
-    return sensorId;
-  }
-
+  /**
+   * Sets the type parameter. Either a sensorType, actuatorType or similar depending on the message.
+   * @param type The type to set.
+   * @return The MessageParameterizer object.
+   */
   public MessageParameterizer setType(String type) {
     this.type = type;
     return this;
   }
 
+  /**
+   * Gets the type parameter. Either a sensorType, actuatorType or similar depending on the message.
+   * @return The type parameter.
+   */
   public String getType() {
     return type;
   }
 
+  /**
+   * Sets the value parameter. Either a sensorValue, actuatorValue or similar depending on the message.
+   * @param value The value to set.
+   * @return The MessageParameterizer object.
+   */
   public MessageParameterizer setValue(String value) {
     this.value = value;
     return this;
   }
 
+  /**
+   * Gets the value parameter. Either a sensorValue, actuatorValue or similar depending on the message.
+   * @return The value parameter.
+   */
   public String getValue() {
     return value;
+  }
+
+  /**
+   * Gets the prefix of the message.
+   * @param prefix The prefix to set.
+   */
+  private void setPrefix(String prefix) {
+    this.prefix = prefix;
+  }
+
+  /**
+   * Gets the prefix of the message.
+   * @return The prefix of the message.
+   */
+  public String getPrefix() {
+    return prefix;
   }
 
   /**
@@ -73,11 +116,8 @@ public class MessageParameterizer {
     if (nodeId != null) {
       message += NODE_ID_PARAMETER + "=" + nodeId + "&";
     }
-    if (actuatorId != null) {
-      message += ACTUATOR_ID_PARAMETER + "=" + actuatorId + "&";
-    }
-    if (sensorId != null) {
-      message += SENSOR_ID_PARAMETER + "=" + sensorId + "&";
+    if (itemId != null) {
+      message += ITEM_ID_PARAMETER + "=" + itemId + "&";
     }
     if (type != null) {
       message += TYPE_PARAMETER + "=" + type + "&";
@@ -95,7 +135,8 @@ public class MessageParameterizer {
    */
   public MessageParameterizer deparameterize(String message) throws IllegalArgumentException {
     clearParameters();
-//    String prefix = message.substring(0, message.indexOf("?"));
+    String prefix = message.substring(0, message.indexOf("?"));
+    setPrefix(prefix);
     String[] parameters = message.substring(message.indexOf("?") + 1).split("&");
     for (String parameter : parameters) {
       String[] keyValue = parameter.split("=");
@@ -103,11 +144,8 @@ public class MessageParameterizer {
         case NODE_ID_PARAMETER:
           setNodeId(keyValue[1]);
           break;
-        case ACTUATOR_ID_PARAMETER:
-          setActuatorId(keyValue[1]);
-          break;
-        case SENSOR_ID_PARAMETER:
-          setSensorId(keyValue[1]);
+        case ITEM_ID_PARAMETER:
+          setItemId(keyValue[1]);
           break;
         case TYPE_PARAMETER:
           setType(keyValue[1]);
@@ -122,10 +160,12 @@ public class MessageParameterizer {
     return this;
   }
 
+  /**
+   * Sets all parameters to null.
+   */
   private void clearParameters() {
     nodeId = null;
-    actuatorId = null;
-    sensorId = null;
+    itemId = null;
     type = null;
     value = null;
   }
