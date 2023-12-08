05c99e102c4ed72186b12f87b0e0f8b24cea3487
Anders Lund <a_lund_01@hotmail.com>
2023-12-08 13:16:52 +0100

    Bugfix encryption after lint issues

--- src/main/java/no/ntnu/encryption/SymmetricEncryption.java
+++ src/main/java/no/ntnu/encryption/SymmetricEncryption.java
@@ -1,12 +1,14 @@
 package no.ntnu.encryption;
 
 import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.KeySpec;
+import java.util.Arrays;
 
 class SymmetricEncryption {
 
@@ -16,21 +18,38 @@ class SymmetricEncryption {
   private SymmetricEncryption() {
   }
 
-  public static byte[] encryptMessage(String messageToEncrypt, String passPhrase) throws IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-
+  public static byte[] encryptMessage(String messageToEncrypt, String passPhrase) throws IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, ShortBufferException {
     SecretKey secretKey = generateSecretKeyFromPassword(passPhrase);
-    Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
-    return cipher.doFinal(messageToEncrypt.getBytes());
+    Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
+    SecureRandom random = new SecureRandom();
+    byte[] iv = new byte[12];
+    random.nextBytes(iv);
+
+    GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, iv);
+
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey, gcmParameterSpec);
+
+    byte[] ivAndEncrypted = new byte[iv.length + cipher.getOutputSize(messageToEncrypt.getBytes().length)];
+    System.arraycopy(iv, 0, ivAndEncrypted, 0, iv.length);
+
+    int encryptedLength = cipher.doFinal(messageToEncrypt.getBytes(), 0, messageToEncrypt.getBytes().length, ivAndEncrypted, iv.length);
+
+    return Arrays.copyOf(ivAndEncrypted, iv.length + encryptedLength);
   }
 
-  public static String decryptMessage(byte[] encryptedMessage, String passPhrase) throws IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, InvalidKeyException {
+  public static String decryptMessage(byte[] encryptedMessage, String passPhrase) throws IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, InvalidAlgorithmParameterException {
     SecretKey secretKey = generateSecretKeyFromPassword(passPhrase);
-    Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-    cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-    byte[] decryptBytes = cipher.doFinal(encryptedMessage);
+    // Get the GCM parameters from the encrypted message
+    AlgorithmParameters parameters = AlgorithmParameters.getInstance("GCM");
+    parameters.init(new GCMParameterSpec(128, encryptedMessage, 0, 12)); // Adjust the length based on your requirements
+
+    Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+    cipher.init(Cipher.DECRYPT_MODE, secretKey, parameters);
+
+    byte[] decryptBytes = cipher.doFinal(encryptedMessage, 12, encryptedMessage.length - 12);
 
     return new String(decryptBytes);
   }
@@ -50,6 +69,4 @@ class SymmetricEncryption {
 
     return new SecretKeySpec(tmp.getEncoded(), "AES");
   }
-
-
 }
