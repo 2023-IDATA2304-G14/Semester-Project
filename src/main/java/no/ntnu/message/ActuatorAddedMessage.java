02f5aa630e2ee054200ccea8a47d8fc13e6d8629
hermanslettum <hermanokern@outlook.com>
2023-12-09 13:54:41 +0100

    Actuator Added and Remove messages

--- src/main/java/no/ntnu/message/ActuatorAddedMessage.java
+++ src/main/java/no/ntnu/message/ActuatorAddedMessage.java
@@ -1,4 +1,9 @@
 package no.ntnu.message;
 
 public record ActuatorAddedMessage(int nodeId, int actuatorId) {
+    public ActuatorAddedMessage {
+        if (nodeId < 0 || actuatorId < 0) {
+            throw new IllegalArgumentException("Node ID and Actuator ID must be non-negative.");
+        }
+    }
 }
--- src/main/java/no/ntnu/message/ActuatorRemoveMessage.java
+++ src/main/java/no/ntnu/message/ActuatorRemoveMessage.java
@@ -1,4 +1,9 @@
 package no.ntnu.message;
 
 public record ActuatorRemoveMessage(int nodeId, int actuatorId) {
+    public ActuatorRemoveMessage {
+        if (nodeId < 0 || actuatorId < 0) {
+            throw new IllegalArgumentException("Node ID and Actuator ID must be non-negative.");
+        }
+    }
 }
