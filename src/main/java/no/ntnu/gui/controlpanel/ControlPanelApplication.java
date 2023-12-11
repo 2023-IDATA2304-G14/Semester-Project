98638968f107ac45fdb68a89607875de9df7409b
larsbarstad <lmlovold@stud.ntnu.no>
2023-12-11 02:54:56 +0100

    fixed bug with actuatorpane

--- src/main/java/no/ntnu/gui/controlpanel/ControlPanelApplication.java
+++ src/main/java/no/ntnu/gui/controlpanel/ControlPanelApplication.java
@@ -109,11 +109,8 @@ public class ControlPanelApplication extends Application implements GreenhouseEv
 
     setPort();
 
-
-
     mainScene = new Scene(mainLayout, WIDTH, HEIGHT);
     stage.setScene(mainScene);
-
 
     stage.show();
     channel.getNodes();
